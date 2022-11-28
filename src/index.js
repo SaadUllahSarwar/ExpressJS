9a1dc34f691d4c1dec7d0393840a385600cad969
Saad Sarwar <saad.sarwar@heuristicsol.com>
2022-11-28 18:11:31 +0500

    users api

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const noteRouter = require("./routes/noteRoutes");
+const userRouter = require("./routes/userRoutes");
+
+app.use(express.json());
+
+const mongoose = require("mongoose");
+mongoose
+    .connect(
+        "mongodb+srv://admin:admin@cluster0.6maczeq.mongodb.net/?retryWrites=true&w=majority"
+    )
+    .then(() => {
+        app.listen(5000, () => {
+            console.log("server started");
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+app.use("/users", userRouter);
+app.use("/note", noteRouter);
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
